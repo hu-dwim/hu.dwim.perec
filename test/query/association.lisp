7051a124c07beaf014f358eb70e3d841b5f54b40
Tamás Borbély <tomi.borbely@gmail.com>
2007-02-05 16:06:43 +0000

    add '-test' suffix to test classes

--- test/query/association.lisp
+++ test/query/association.lisp
@@ -1,68 +1,68 @@
 (in-package :cl-perec-test)
 
-(defpclass* person ()
+(defpclass* person-test ()
   ((first-name :type (string 50))
    (last-name :type (string 50))))
 
-(defpclass* movie ()
+(defpclass* movie-test ()
   ((title :type (string 50))))
 
-(defpclass* man (person)
+(defpclass* man-test (person-test)
   ())
 
-(defpclass* woman (person)
+(defpclass* woman-test (person-test)
   ())
 
-(defpclass* performer (person)
+(defpclass* performer-test (person-test)
   ())
 
-(defpclass* actor (performer man)
+(defpclass* actor-test (performer-test man-test)
   ())
 
-(defpclass* actress (performer woman)
+(defpclass* actress-test (performer-test woman-test)
   ())
 
 (defassociation*
-  ((:class man :slot wife :type (or null woman))
-   (:class woman :slot husband :type (or null man))))
+  ((:class man-test :slot wife :type (or null woman-test))
+   (:class woman-test :slot husband :type (or null man-test))))
 
 (defassociation*
-  ((:class movie :slot performers :type (set performer))
-   (:class performer :slot movies :type (set movie))))
+  ((:class movie-test :slot performers :type (set performer-test))
+   (:class performer-test :slot movies :type (set movie-test))))
 
 (defixture fill-data-2
   (with-transaction
-    (purge-objects 'movie)
-    (purge-objects 'person)
-    (let ((oceans-twelwe (make-instance 'movie :title "Ocean's Twelwe"))
-          (mr&mrs-smith (make-instance 'movie :title "Mr. & Mrs. Smith"))
-          (george-clooney (make-instance 'actor :first-name "George" :last-name "Clooney"))
-          (brad-pitt (make-instance 'actor :first-name "Brad" :last-name "Pitt"))
-          (angelina-jolie (make-instance 'actress :first-name "Angelina" :last-name "Jolie")))
+    (purge-objects 'movie-test)
+    (purge-objects 'person-test)
+    (let ((oceans-twelwe (make-instance 'movie-test :title "Ocean's Twelwe"))
+          (mr&mrs-smith (make-instance 'movie-test :title "Mr. & Mrs. Smith"))
+          (george-clooney (make-instance 'actor-test :first-name "George" :last-name "Clooney"))
+          (brad-pitt (make-instance 'actor-test :first-name "Brad" :last-name "Pitt"))
+          (angelina-jolie (make-instance 'actress-test :first-name "Angelina" :last-name "Jolie")))
       (setf (performers-of oceans-twelwe) (list george-clooney brad-pitt)
             (performers-of mr&mrs-smith) (list brad-pitt angelina-jolie)
             (wife-of brad-pitt) angelina-jolie))))
 
 (deftest test/query/select/association-n-m ()
   (test-query (:select-count 1 :record-count 4 :fixture fill-data-2)
-    (select ((m movie) (p performer))
+    (select ((m movie-test) (p performer-test))
       (assert (member p (performers-of m)))
       (collect (title-of m) (first-name-of p) (last-name-of p)))))
 
 (deftest test/query/select/association-1-1 ()
   (test-query (:select-count 1 :record-count 1 :fixture fill-data-2)
-    (select ((m man) (w woman))
+    (select ((m man-test) (w woman-test))
       (assert (eq (wife-of m) w))
       (collect m w)))
   (test-query (:select-count 1 :record-count 1 :fixture fill-data-2)
-    (select ((m man) (w woman))
+    (select ((m man-test) (w woman-test))
       (assert (eq (husband-of w) m))
       (collect m w))))
 
 (deftest test/query/select/association-chain ()
   (test-query (:select-count 1 :record-count 1 :fixture fill-data-2)
-    (select ((m man))
+    (select ((m man-test))
       (assert (not (null (wife-of m))))
-      (assert (typep (husband-of (wife-of m)) 'performer))
+      (assert (typep (husband-of (wife-of m)) 'performer-test))
       (assert (> (length (movies-of (husband-of (wife-of m)))) 0))
       (collect m))))
