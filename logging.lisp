edc22ec6660f0d870e5e7fb450cc4f3dea4ffead
Levente Mészáros <levente.meszaros@gmail.com>
2007-10-27 13:35:52 +0000

    Logging.

--- logging.lisp
+++ logging.lisp
@@ -6,11 +6,17 @@
 
 (in-package :cl-perec)
 
-(defvar *log-level* (if *load-as-production-p* +info+ +debug+))
+(defvar *runtime-log-level* (if *load-as-production-p* +info+ +debug+))
 (defvar *compile-time-log-level* (if *load-as-production-p* +debug+ +dribble+))
 
+(deflogger plog ()
+  :level *runtime-log-level*
+  :compile-time-level *compile-time-log-level*
+  :appenders ((debug-only*
+                (make-instance 'brief-stream-log-appender :stream *debug-io*))))
+
 (deflogger qlog ()
-  :level *log-level*
+  :level *runtime-log-level*
   :compile-time-level *compile-time-log-level*
   :appenders ((debug-only*
                 (make-instance 'brief-stream-log-appender :stream *debug-io*))))
