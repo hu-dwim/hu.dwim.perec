e549400883ad88f99839158bea009a8534a25cfe
Levente Mészáros <levente.meszaros@gmail.com>
2007-11-12 12:55:20 +0000

    Added command.

--- persistence/class-t.lisp
+++ persistence/class-t.lisp
@@ -114,6 +114,7 @@
 ;; TODO: disallow changing the t parameter in a transaction
 (defmethod call-in-transaction :around (database (transaction transaction-t-mixin) function)
   (declare (ignore database function))
+  ;; TODO: this will always do a select now() but it should be lazy
   (with-default-t
     (call-next-method)))
 
