951fac54a64a8dcad0c4f9b10392fd35c691ba8f
Tamás Borbély <tomi.borbely@gmail.com>
2008-02-04 10:54:41 +0000

    update tesites test

--- test/tesites/time-dependent.lisp
+++ test/tesites/time-dependent.lisp
@@ -46,7 +46,7 @@
         (bind ((instance (make-instance 'time-dependent-test)))
           (setf (population-of instance) 1000)
           (is (= (update-counter-of (command-counter-of *transaction*)) 1))
-          (is (= (insert-counter-of (command-counter-of *transaction*)) (+ 2 3)))
+          (is (= (insert-counter-of (command-counter-of *transaction*)) (+ 1 3)))
           instance)
       (let ((update-counter (update-counter-of (command-counter-of *transaction*)))
             (insert-counter (insert-counter-of (command-counter-of *transaction*))))
