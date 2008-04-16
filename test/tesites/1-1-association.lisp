0a9cca3523cca63ee218c65b260b1dcb149ca453
Tamás Borbély <tomi.borbely@gmail.com>
2008-04-16 07:43:15 +0000

    t-s 1-1 association tests

--- test/tesites/1-1-association.lisp
+++ test/tesites/1-1-association.lisp
@@ -18,8 +18,28 @@
   ())
 
 (defassociation*
-  ((:class tesites-sister-test :slot parent :type (or null tesites-brother-test))
-   (:class tesites-brother-test :slot children :type (set tesites-sister-test)))
+  ((:class tesites-sister-test :slot brother :type (or null tesites-brother-test))
+   (:class tesites-brother-test :slot sister :type (or null tesites-sister-test)))
   (:time-dependent #t)
   (:temporal #t))
 
+(deftest test/tesites/association/1-1/t-not-specified ()
+  (with-transaction
+    (with-validity "2007"
+      (signals unbound-variable (sister-of (make-instance 'tesites-brother-test))))))
+
+(deftest test/tesites/association/1-1/initial-value/null ()
+  (with-transaction
+    (with-default-t
+      (is (null (sister-of (make-instance 'tesites-brother-test))))
+      (is (null (brother-of (make-instance 'tesites-sister-test)))))))
+
+(deftest test/tesites/association/1-1/store-value/1 ()
+  (with-transaction
+    (with-validity "2007-01-01"
+      (with-default-t
+        (bind ((brother (make-instance 'tesites-brother-test))
+               (sister (make-instance 'tesites-sister-test)))
+          (setf (sister-of brother) sister)
+          (is (eq sister (sister-of brother)))
+          (is (eq brother (brother-of sister))))))))
