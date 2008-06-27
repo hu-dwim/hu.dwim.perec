0b695a369eaaa85233e95e979f2c02a1ba04aa2e
Levente Mészáros <levente.meszaros@gmail.com>
2008-06-27 15:21:41 +0000

    Bah the accessor is not yet available during loading fasls, so use slot-value instead.

--- persistence/mop.lisp
+++ persistence/mop.lisp
@@ -158,7 +158,7 @@
   (mapc #L(ensure-slot-reader* class !1)
         (collect-if #L(set-type-p* (canonical-type-of !1))
                     (persistent-effective-slots-of class)))
-  (setf (persistent-p (class-prototype class)) #f))
+  (setf (slot-value (class-prototype class) 'persistent) #f))
 
 (defmethod compute-slots :after ((class persistent-class))
   "Invalidates the cached slot values whenever the effective slots are recomputed, so that all dependent computed state will be invalidated and recomputed when requested."
