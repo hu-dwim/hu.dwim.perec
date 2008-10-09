2767b9b16f135b695864a1ede3d1b1ba15f58068
Levente Mészáros <levente.meszaros@gmail.com>
2008-10-09 08:51:21 +0000

    Minor fixes to dimensional associations.

--- dimensional/association.lisp
+++ dimensional/association.lisp
@@ -42,7 +42,7 @@
     (compute-as (first (columns-of (action-slot-of -self-))))
     :type column)
    (h-slot-name
-    (compute-as (concatenate-symbol "t-" (slot-definition-name (other-association-end-of -self-))))
+    (compute-as (concatenate-symbol "d-" (slot-definition-name (other-association-end-of -self-))))
     :type symbol)
    (h-slot
     (compute-as (find-persistent-slot (h-class-of -self-) (h-slot-name-of -self-) :otherwise nil)))
@@ -62,20 +62,18 @@
 (def method expand-defassociation-form :around ((metaclass null) association-ends options)
   (bind ((specified-metaclass (second (find :metaclass options :key #'first)))
          (processed-options
-          #+nil ;; TODO:
           (if (and (not specified-metaclass)
-                   (or (find :temporal options :key #'first)
-                       (find :time-dependent options :key #'first)))
+                   (or (find :dimensions options :key #'first)))
               (append options '((:metaclass persistent-association-d)))
               options)))
     (call-next-method metaclass association-ends processed-options)))
 
 (def method expand-defassociation-form ((metaclass persistent-association-d) association-ends options)
   (with-decoded-association-ends association-ends
-    (bind ((dimensions (find :dimensions options :key #'first))
+    (bind ((dimensions (second (find :dimensions options :key #'first)))
            (superclasses
-            ;; TODO:
-            )
+            (iter (for dimension :in dimensions)
+                  (adjoining (dependent-object-name-of (find-dimension dimension)))))
            (processed-options (remove-if #L(member (first !1) '(:temporal :time-dependent)) options))
            (processed-association-ends
             (mapcar #L(append (when dimensions
@@ -92,8 +90,8 @@
          (defpclass* ,association-name ,superclasses
            ,slot-definitions)
          (defassociation*
-           ((:class ,association-name :slot ,(concatenate-symbol "t-" secondary-slot *package*) :type (or null ,primary-class))
+           ((:class ,association-name :slot ,(concatenate-symbol "d-" secondary-slot *package*) :type (or null ,primary-class))
             (:class ,primary-class :slot ,(concatenate-symbol "h-" primary-slot *package*) :type (set ,association-name))))
          (defassociation*
-           ((:class ,association-name :slot ,(concatenate-symbol "t-" primary-slot *package*) :type (or null ,secondary-class))
+           ((:class ,association-name :slot ,(concatenate-symbol "d-" primary-slot *package*) :type (or null ,secondary-class))
             (:class ,secondary-class :slot ,(concatenate-symbol "h-" secondary-slot *package*) :type (set ,association-name))))))))
