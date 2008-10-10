276f9f3d92023c74b4242d80007e501e508127af
Tamás Borbély <tomi.borbely@gmail.com>
2008-10-10 08:01:19 +0000

    store dimensional slots

--- dimensional/slot-value.lisp
+++ dimensional/slot-value.lisp
@@ -5,3 +5,117 @@
 ;;; See LICENCE for details.
 
 (in-package :cl-perec)
+
+(def special-variable *simplify-d-values* #f)
+
+(def (function io) simplify-d-value (d-value)
+  (if (and *simplify-d-values* (single-d-value-p d-value))
+      (single-d-value d-value)
+      d-value))
+
+(defcondition* unbound-slot-d (unbound-slot)
+  ((coordinates :type list))
+  (:report (lambda (condition stream)
+             (format stream "The slot ~S is unbound in the object ~S with coordinates ~S."
+                     (cell-error-name condition)
+                     (unbound-slot-instance condition)
+                     (coordinates-of condition)))))
+
+(def function slot-unbound-d (instance slot coordinates)
+  (error 'unbound-slot-d
+         :name (slot-definition-name slot)
+         :instance instance
+         :coordinates coordinates))
+
+(def (function io) collect-coordinates-from-variables (dimensions)
+  (iter (for dimension :in dimensions)
+        (typecase dimension
+          (ordering-dimension
+           (assert (and (boundp (begin-coordinate-name-of dimension))
+                         (boundp (end-coordinate-name-of dimension))))
+           (collect (cons
+                     (symbol-value (begin-coordinate-name-of dimension))
+                     (symbol-value (end-coordinate-name-of dimension)))))
+          (t
+           (assert (boundp (coordinate-name-of dimension)))
+           (collect (symbol-value (coordinate-name-of dimension)))))))
+
+(def (function io) slot-boundp-or-value-using-class-d (class instance slot coordinates)
+  (assert-instance-slot-correspondence)
+  (bind ((persistent (persistent-p instance))
+         (cache-p (and *cache-slot-values* (cache-p slot))))
+    (assert-instance-access instance persistent)
+    (bind (((:values slot-value-cached cached-value) (slot-value-cached-p instance slot)))
+      (when (or (not persistent)
+                slot-value-cached)
+        (if (unbound-slot-marker-p cached-value)
+            (return-from slot-boundp-or-value-using-class-d +unbound-slot-marker+)
+            (bind ((d-value (value-at-coordinates cached-value coordinates)))
+              (when (covering-d-value-p d-value coordinates)
+                (return-from slot-boundp-or-value-using-class-d (simplify-d-value d-value))))))
+      (if persistent
+          (aprog1 (restore-slot class instance slot :coordinates coordinates)
+            (when (or (not persistent) cache-p)
+              (if (d-value-p cached-value)
+                  (setf (into-d-value cached-value) it)
+                  (setf (underlying-slot-value-using-class class instance slot) it))))
+          (return-from slot-boundp-or-value-using-class-d +unbound-slot-marker+)))))
+
+(def (function io) (setf slot-boundp-or-value-using-class-d) (new-value class instance slot)
+  (assert-instance-slot-correspondence)
+  (bind ((persistent (persistent-p instance))
+         (cache-p (and *cache-slot-values* (cache-p slot)))
+         (new-value (if (not (d-value-p new-value))
+                        (make-single-d-value (dimensions-of slot)
+                                             (collect-coordinates-from-variables (dimensions-of slot))
+                                             new-value)
+                        new-value)))
+    (assert-instance-access instance persistent)
+    (when persistent
+      (store-slot class instance slot new-value))
+    (when (or (not persistent) cache-p)
+      (bind (((:values slot-value-cached cached-value) (slot-value-cached-p instance slot)))
+        (if (and slot-value-cached (d-value-p cached-value))
+            (setf (into-d-value cached-value) new-value)
+            (setf (underlying-slot-value-using-class class instance slot) new-value)))))
+  new-value)
+
+(defmethod slot-value-using-class ((class persistent-class)
+                                   (instance persistent-object)
+                                   (slot persistent-effective-slot-definition-d))
+  "Reads the slot value from the database or the cache."
+  (bind ((coordinates (collect-coordinates-from-variables (dimensions-of slot)))
+         (value (slot-boundp-or-value-using-class-d class instance slot coordinates)))
+    (flet ((check-value (value coordinates)
+             (when (unbound-slot-marker-p value)
+               (slot-unbound-d instance slot coordinates))))
+      (if (d-value-p value)
+          (iter (for c-value :in (c-values-of value))
+                (check-value (value-of c-value) (coordinates-of c-value)))
+          (check-value value coordinates))
+      value)))
+
+(defmethod (setf slot-value-using-class) (new-value
+                                          (class persistent-class)
+                                          (instance persistent-object)
+                                          (slot persistent-effective-slot-definition-d))
+  "Writes the new slot value to the database and the cache."
+  (setf (slot-boundp-or-value-using-class-d class instance slot) new-value))
+
+(defmethod slot-boundp-using-class ((class persistent-class)
+                                    (instance persistent-object)
+                                    (slot persistent-effective-slot-definition-d))
+  "Reads boundness from the database or the cache."
+  (bind ((coordinates (collect-coordinates-from-variables (dimensions-of slot)))
+         (value (slot-boundp-or-value-using-class-d class instance slot coordinates)))
+    (if (d-value-p value)
+        (iter (for c-value :in (c-values-of value))
+              (always (not (unbound-slot-marker-p (value-of c-value)))))
+        (not (unbound-slot-marker-p value)))))
+
+(defmethod slot-makunbound-using-class ((class persistent-class)
+                                        (instance persistent-object)
+                                        (slot persistent-effective-slot-definition-d))
+  "Writes boundness to the database and the cache."
+  (setf (slot-boundp-or-value-using-class-d class instance slot) +unbound-slot-marker+)
+  instance)
--- dimensional/store.lisp
+++ dimensional/store.lisp
@@ -14,12 +14,60 @@
 ;;;
 ;;; Slot access
 ;;;
-(def method restore-slot ((d-class persistent-class-d) (d-instance persistent-object-d) (d-slot persistent-effective-slot-definition-d) &key coordinates)
+(def method restore-slot ((d-class persistent-class-d)
+                           (d-instance persistent-object-d)
+                          (d-slot persistent-effective-slot-definition-d)
+                          &key coordinates)
   (bind (((d-slot-default-value . has-default-p) (default-value-for-type-of d-slot))
          (default-value (if has-default-p d-slot-default-value +unbound-slot-marker+))
          (dimensions (dimensions-of d-slot))
          (records (select-slot-values-with-dimensions d-class d-instance d-slot coordinates)))
     (make-d-value-from-records records default-value dimensions coordinates)))
+
+(def method store-slot ((d-class persistent-class-d)
+                         (d-instance persistent-object-d)
+                        (d-slot persistent-effective-slot-definition-d)
+                        value)
+  (assert (d-value-p value))
+  (iter (for (v coordinates) :in-d-value value)
+        (check-slot-value-type d-instance d-slot v))
+
+  ;; this lock ensures that
+  ;; the insert/update operations on the h-table are serialized properly.
+  (lock-slot d-instance d-slot)
+
+  (iter (for (v coordinates) :in-d-value value)
+        (store-slot-t* d-class d-instance d-slot v coordinates)))
+
+(def method lock-slot ((d-instance persistent-object) (d-slot persistent-effective-slot-definition-d) &key (wait t))
+  (bind ((h-slot (h-slot-of d-slot))
+         (table (name-of (table-of h-slot))))
+    (sql-lock-table :table table
+                    :mode :exclusive
+                    :wait wait)))
+
+(def function store-slot-t* (d-class d-instance d-slot value coordinates)
+
+  (bind ((dimensions (dimensions-of d-slot))
+         (inheriting-dimension (find-if [typep !1 'inheriting-dimension] dimensions))
+         ((d-slot-default-value . has-default-p) (default-value-for-type-of d-slot))
+         (update-count))
+
+    ;; do not store the default value of the slot in a transient instance except if temporal.
+    ;; restore-slot interprets missing h-instances as the default value
+    (when (and (not (persistent-p d-instance))
+               has-default-p
+               (equal value d-slot-default-value) ; FIXME equal
+               (null inheriting-dimension))
+      (return-from store-slot-t*))
+
+    ;; first try to update value in the h-instance having the same coordinates
+    (setf update-count (update-h-instance-slot-value d-class d-instance d-slot value coordinates))
+
+    ;; if the update is not succeeded then insert value with t and validity except if it
+    ;; is the default value of a non-temporal slot
+    (when (zerop update-count)
+      (insert-h-instance d-class d-instance d-slot value coordinates))))
 
 ;;;
 ;;; d-value builders
@@ -51,12 +99,6 @@
   (assert (and (typep dimension 'ordering-dimension) (not (typep dimension 'inheriting-dimension))))
   (format-symbol "~A-END" (symbol-package (name-of dimension)) (name-of dimension)))
 
-(def function slot-names-of (dimension)
-  (typecase dimension
-    (inheriting-dimension (list (slot-name-of dimension)))
-    (ordering-dimension (list (begin-slot-name-of dimension) (end-slot-name-of dimension)))
-    (dimension (list (slot-name-of dimension)))))
-
 (def (function io) dimension-equal (dimension)
   (case (the-type-of dimension)
     ((time date timestamp) 'timestamp=)
@@ -80,23 +122,24 @@
          (h-slot (h-slot-of d-slot))
          (h-slot-name (slot-definition-name h-slot))
          (dimensions (dimensions-of d-slot))
-         (query (make-query nil `(d-instance ,(mapcar #'name-of dimensions))))
+         (query (make-query `(select ((slot-value h-instance ',h-slot-name))
+                               (from (h-instance ,h-class-name))
+                               (where (and (eq (d-instance-of h-instance) d-instance)
+                                           (or
+                                            (not (slot-boundp h-instance ',h-slot-name))
+                                            (not (eq (slot-value h-instance ',h-slot-name)
+                                                     ,+h-unused-slot-marker+))))))
+                            `(d-instance)))
          result)
 
-    (add-query-variable query 'h-instance)
-    (add-collect query `(slot-value h-instance ',h-slot-name))
-    (add-assert query `(typep h-instance ,h-class-name))
-    (add-assert query `(eq (d-instance-of h-instance) d-instance))
-    (add-assert query `(or
-                        (not (slot-boundp h-instance ',h-slot-name))
-                        (not (eq (slot-value h-instance ',h-slot-name) ,+h-unused-slot-marker+))))
-    
     (iter (for dimension :in dimensions)
           (for coordinate :in coordinates)
           (for interval-p = (when (typep dimension 'ordering-dimension)
                               (assert (funcall (dimension-less-or-equal dimension)
                                                (car coordinate) (cdr coordinate)))
                               (funcall (dimension-less dimension) (car coordinate) (cdr coordinate))))
+
+          (add-lexical-variable query (name-of dimension))
 
           (etypecase dimension
 
@@ -181,3 +224,64 @@
                  result)))))
 
     result))
+
+(defun update-h-instance-slot-value (d-class d-instance d-slot value coordinates)
+
+  (bind ((h-class (h-class-of d-class))
+         (h-slot (h-slot-of d-slot))
+         (dimensions (dimensions-of d-slot))
+         (query (make-query
+                 `(update (h-instance ,h-class)
+                    (set (slot-value h-instance ',(slot-definition-name h-slot)) value)
+                    (from (h-instance ,(class-name h-class)))
+                    (where (and (eq (d-instance-of h-instance) d-instance))))
+                 '(d-instance value))))
+
+    (iter (for dimension :in dimensions)
+          (add-lexical-variable query (name-of dimension))
+          (etypecase dimension
+            (inheriting-dimension
+             (add-assert query `(,(dimension-equal dimension)
+                                  (slot-value h-instance ',(slot-name-of dimension))
+                                  (car ,(name-of dimension)))))
+            (ordering-dimension
+             (add-assert query `(,(dimension-equal dimension)
+                                  (slot-value h-instance ',(begin-slot-name-of dimension))
+                                  (car ,(name-of dimension))))
+             (add-assert query `(,(dimension-equal dimension)
+                                  (slot-value h-instance ',(end-slot-name-of dimension))
+                                  (cdr ,(name-of dimension)))))
+            (dimension
+             (add-assert query `(,(dimension-equal dimension)
+                                  (slot-value h-instance ',(slot-name-of dimension))
+                                  ,(name-of dimension))))))
+
+    ;; generate condition for version check
+    #+nil
+    (add-assert query `(eq h-instance (select ((max h-instance))
+                                        (from (h-instance ,(class-name h-class)))
+                                        (where (and (eq (d-instance-of h-instance) d-instance))))))
+
+    (prog1-bind count (execute-query query d-instance value coordinates)
+      (assert (<= count 1) nil "Inconsistent database"))))
+
+(def function insert-h-instance (d-class d-instance d-slot value coordinates)
+  (bind ((h-class (h-class-of d-class)))
+    (flet ((initarg-of (slot-name)
+             (aprog1 (first (slot-definition-initargs (find-persistent-slot h-class slot-name)))
+               (assert it))))
+      (apply 'make-instance
+            h-class
+            :d-instance d-instance
+            (initarg-of (slot-definition-name d-slot)) value
+            (iter (for dimension :in (dimensions-of d-slot))
+                  (for coordinate :in coordinates)
+                  (etypecase dimension
+                    (inheriting-dimension
+                     (assert (coordinate= (car coordinate) (cdr coordinate)))
+                     (collect (list (initarg-of (slot-name-of dimension)) (car coordinate))))
+                    (ordering-dimension
+                     (collect (list (initarg-of (begin-slot-name-of dimension)) (car coordinate)))
+                     (collect (list (initarg-of (end-slot-name-of dimension)) (cdr coordinate))))
+                    (dimension
+                     (collect (list (initarg-of (slot-name-of dimension)) coordinate)))))))))
