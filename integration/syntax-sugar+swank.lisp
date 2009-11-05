1dea41ea6187d91d1d9b124dd64ab5069cdb9ef5
Attila Lendvai <attila@lendvai.name>
2009-11-05 12:45:48 +0000

    some steps for the inspector customizations

--- integration/syntax-sugar+swank.lisp
+++ integration/syntax-sugar+swank.lisp
@@ -9,34 +9,32 @@
 ;;;;;;
 ;;; Slime inspector integration
 
-#+slime-dwim-branch
+#+hu.dwim.slime
 (progn
-  (defmethod swank::inspect-slot-for-emacs ((class persistent-class)
-                                            (instance persistent-object)
-                                            (slot persistent-effective-slot-definition))
-    (if (debug-persistent-p instance)
-        `(,@(if (slot-value-cached-p instance slot)
-                `("Cached, value is " (:value ,(standard-instance-access instance (slot-definition-location slot)))
-                  " "
-                  (:action "[invalidate cache]" ,(lambda () (invalidate-cached-slot instance slot))))
-                `("Not cached"
-                  " "
-                  (:action "[read in]" ,(lambda () (slot-value-using-class class instance slot)))))
+
+(defmethod swank::inspect-slot-for-emacs ((class persistent-class)
+                                          (instance persistent-object)
+                                          (slot persistent-effective-slot-definition))
+  (if (debug-persistent-p instance)
+      `(,@(if (slot-value-cached-p instance slot)
+              `("Cached, value is " (:value ,(standard-instance-access instance (slot-definition-location slot)))
+                                    " "
+                                    (:action "[invalidate cache]" ,(lambda () (invalidate-cached-slot instance slot))))
+              `("Not cached"
+                " "
+                (:action "[read in]" ,(lambda () (slot-value-using-class class instance slot)))))
           " "
           (:action "[make unbound]" ,(lambda () (slot-makunbound-using-class class instance slot))))
-        (call-next-method)))
+      (call-next-method)))
 
-  (defmethod swank::inspect-for-emacs ((instance persistent-object) inspector)
-    (bind ((result (multiple-value-list (call-next-method))))
-      (if (= (length result) 1)
-          (progn
-            (setf result (first result))
-            (bind ((content (getf result :content)))
-              (setf (getf result :content)
-                    (append `("Transaction: " (:value ,(when (instance-in-transaction-p instance) (transaction-of instance))) (:newline))
-                            content))
-              (setf (getf result :title)
-                    (if (debug-persistent-p instance) "A persistent instance" "A transient instance"))
-              result))
-          ;; we do nothing with the old inspect protocol...
-          (values-list result)))))
+(defmethod swank::inspect-for-emacs ((instance persistent-object) inspector)
+  (bind ((result (multiple-value-list (call-next-method))))
+    (setf result (first result))
+    (bind ((content (getf result :content)))
+          (setf (getf result :content)
+                (append `("Transaction: " (:value ,(when (instance-in-transaction-p instance) (transaction-of instance))) (:newline))
+                        content))
+          (setf (getf result :title)
+                (if (debug-persistent-p instance) "A persistent instance" "A transient instance"))
+          result)))
+)
