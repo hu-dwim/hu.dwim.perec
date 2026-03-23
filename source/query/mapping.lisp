6eca1d9b175344cef90b36f1d9c3edb75ed2e628
Tamas Borbely <tomi.borbely@gmail.com>
2026-03-23 18:09:07 +0100

    Revert "fix: don't lie about the return type of oid-of for the query compiler"
    This reverts commit e68682770650696120f30bed7f6a093cf30b5727.

--- source/query/mapping.lisp
+++ source/query/mapping.lisp
@@ -488,7 +488,7 @@
 ;;;
 (def query-function oid-of (instance)
   ""
-  (declare (persistent-type (function (persistent-object) oid)))
+  (declare (persistent-type (function (persistent-object) number)))
   instance)
 
 ;;;
